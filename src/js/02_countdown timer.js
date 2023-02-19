de6b0df024fdd8c2916f0c20d2ad965dd68a63fc
Andriy Dumenko <101402719+dumenko111@users.noreply.github.com>
2023-02-19 16:14:53 +0200

    countdown-timer

--- src/js/02_countdown timer.js
+++ src/js/02_countdown timer.js
@@ -1,34 +1,109 @@
-class CountdownTimer {
-
+refs = {
+  days: document.querySelector('span[data-value="days"]'),
+  hours: document.querySelector('span[data-value="hours"]'),
+  mins: document.querySelector('span[data-value="mins"]'),
+  secs: document.querySelector('span[data-value="secs"]'),
 }
 
-const countdownTimer = new CountdownTimer({
+
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector
+    this.targetDate = targetDate
+  }
+
+  start() {
+    this.intervalId = setInterval(() => {
+      const currentDate = Date.now()
+      console.log(currentDate)
+      const deltaTime = this.targetDate - currentDate;
+      this.updateCounter(deltaTime)
+    }, 1000)
+  }
+
+  updateCounter(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    console.log(days, hours, mins, secs)
+ 
+    refs.days.textContent = `${days}`;
+    refs.hours.textContent = `${hours}`;
+    refs.mins.textContent = `${mins}`;
+    refs.secs.textContent = `${secs}`;
+  }
+
+  pad(value) {
+    return String(value).padStart(2, '0')
+  }
+  }
+  
+
+const timer = new CountdownTimer({
   selector: '#timer-1',
-  targetDate: new Date('Jul 17, 2019'),
+  targetDate: new Date("Dec 31, 2023"),// дата до якої буде працювати таймер. 👀треба було б зробити, що б в кінці він виключився
 });
+timer.start()
 
 
-/*
- * Дні, що залишилися: ділимо значення UTC на 1000 * 60 * 60 * 24, кількість
- * мілісекунд в один день (мілісекунди * секунди * хвилини * години)
- */
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
 
-/*
- * Решта годин: отримуємо залишок від попереднього розрахунку за допомогою оператора
- * залишку% і ділимо його на кількість мілісекунд в одній годині
- * (1000 * 60 * 60 = мілісекунди * хвилини * секунди)
- */
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+// /*
+//  * Дні, що залишилися: ділимо значення UTC на 1000 * 60 * 60 * 24, кількість
+//  * мілісекунд в один день (мілісекунди * секунди * хвилини * години)
+//  */
+// const days = Math.floor(time / (1000 * 60 * 60 * 24));
 
-/*
- * Решта хвилин: отримуємо хвилини, що залишилися і ділимо їх на кількість
- * мілісекунд в одній хвилині (1000 * 60 = мілісекунди * секунди)
- */
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+// /*
+//  * Решта годин: отримуємо залишок від попереднього розрахунку за допомогою оператора
+//  * залишку % і ділимо його на кількість мілісекунд в одній годині
+//  * (1000 * 60 * 60 = мілісекунди * хвилини * секунди)
+//  */
+// const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 
-/*
- * Решта секунд: отримуємо секунди, які залишилися і ділимо їх на кількість
- * миллисекунд в одной секунде (1000)
- */
-const secs = Math.floor((time % (1000 * 60)) / 1000);
+// /*
+//  * Решта хвилин: отримуємо хвилини, що залишилися і ділимо їх на кількість
+//  * мілісекунд в одній хвилині (1000 * 60 = мілісекунди * секунди)
+//  */
+// const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+
+// /*
+//  * Решта секунд: отримуємо секунди, які залишилися і ділимо їх на кількість
+//  * миллисекунд в одной секунде (1000)
+//  */
+// const secs = Math.floor((time % (1000 * 60)) / 1000);
+
+
+
+
+
+
+
+
+
+
+// const date1 = new Date('Jul 17, 2019')
+// const date2 = new Date('Jul 17, 2022')
+
+// console.log(date2 -  date1)
+
+// let intervalId = null
+
+
+// intervalId = setInterval(() => {
+//   const date1 = new Date("March 01, 2030 00:00:00")
+//   const date2 = new Date("March 12, 2030 11:11:11")
+//   // const deltaTime = date2 - date1
+//   // console.log(updateCounter1(deltaTime))
+// }, 1000)
+
+
+// function updateCounter1(time) {
+//     const days = Math.floor(time / (1000 * 60 * 60 * 24));
+//     const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//     const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+//     const secs = Math.floor((time % (1000 * 60)) / 1000);
+  
+//   return {days, hours, mins, secs}
+// }
